e7b519202db8182e36f5d28c8966128d467040ae
bivisito <92517765+bivisito@users.noreply.github.com>
2022-01-30 20:28:46 +0200

    Removed Fahrenheit conversion and fixed warnings

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -1,42 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherTemp(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temp">{Math.round(props.celsius)}</span>
-        <span className="units">
-          °C |{" "}
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <div className="WeatherTemperature">
-        <span className="temp">{Math.round(fahrenheit)}</span>
-        <span className="units">
-          <a href="/" onClick={showCelsius}>
-            °C
-          </a>
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temp">{Math.round(props.celsius)}</span>
+      <span className="units">°C</span>
+    </div>
+  );
 }
