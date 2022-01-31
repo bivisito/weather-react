4735bd884fc2feff677a4f63a1497b200935085e
bivisito <92517765+bivisito@users.noreply.github.com>
2022-01-31 20:23:22 +0200

    Removed city links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import CityLinks from "./CityLinks";
 import Weather from "./Weather";
 import Footer from "./Footer";
 
@@ -9,8 +8,6 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="weather-app">
-          <CityLinks />
-          <br />
           <Weather defaultCity="Toronto" />
         </div>
         <Footer />
